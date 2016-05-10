6f5fe2b0327c15540870e22deea95c90df9a53ae
Bruce McPherson <bruce@mcpher.com>
2016-05-10 17:59:17 +0100

    updated by GasGit automation

---
+++ scripts/App.js
@@ -0,0 +1,31 @@
+/**
+* this is initialized client side
+* and is used to control the App functions
+* and hold globals values
+* @namespace App
+*/
+var App = (function (ns) {
+  
+  ns.globals = {
+    bigQuery: {
+      projectId:'lateral-command-416',
+      dataStore:'jeopardydata',
+      table:'questions'
+    },
+    oauth: {
+      packageName:'bigquery_serviceaccount',
+      props:PropertiesService.getScriptProperties()
+    }
+  };
+  
+  ns.init = function () {
+    ns.goa = cGoa.GoaApp.createGoa(
+      ns.globals.oauth.packageName, 
+      ns.globals.oauth.props
+    ).execute();
+  
+  };
+  
+  return ns;
+}) (App || {});
+
